ebd23310bd0f7a7bac76c10d2c1d820adaac7d10
aleksandrbagrov <101454330+aleksandrbagrov@users.noreply.github.com>
2023-03-05 00:54:44 +0200

    modify assignment 2023-02-17

--- Assignmen_2023_02_17/index.js
+++ Assignmen_2023_02_17/index.js
@@ -7,7 +7,7 @@ const games = [
     {
         image: './assets/images/1.jpg',
         name: 'Terraria Version 1.4.3',
-        price: 10.00,
+        price: 10.99,
     },
     {
         image: './assets/images/2.jpg',
@@ -31,6 +31,13 @@ const games = [
     },
 ]
 
+const tvOn = () => {
+    box.style.background = '#b5b5eb';
+    screen.innerHTML = '<h1>Welcome!</h1>';
+    screen.innerHTML += '<div><ul><li>TV Program</li><li>Back to Welcome screen</li><li>Games to Buy</li><li>Google</li><li>On / Off</li></ul></div>';
+    monitor.onOff = true;
+}
+
 document.querySelector('.box .content').innerHTML = '';
 const box = document.querySelector('.box');
 let screen = document.querySelector('.box .content');
@@ -41,10 +48,7 @@ document.querySelector('.eventListenerOnOff').addEventListener('click', function
 
     console.log(`1 console ${box.style.background}`);
     if (!monitor.onOff) {
-        box.style.background = 'blue';
-        screen.innerHTML = '<h1>Welcome!</h1>';
-        screen.innerHTML += '<div><ul><li>TV Program</li><li>Back to Welcome screen</li><li>Games to Buy</li><li>Google</li></ul></div>';
-        monitor.onOff = true;
+        tvOn();
     } else {
         box.style.background = 'black';
         screen.innerHTML = '<h1></h1>';
@@ -63,19 +67,17 @@ document.querySelector('.eventListenerTVProg').addEventListener('click', functio
 
 document.querySelector('.eventListenerBack').addEventListener('click', function () {
     if (monitor.onOff) {
-        box.style.background = 'blue';
-        screen.innerHTML = '<h1>Welcome!</h1>';
-        screen.innerHTML += '<div><ul><li>TV Program</li><li>Back to Welcome screen</li><li>Games to Buy</li><li>Google</li></ul></div>';
-        monitor.onOff = true;
+        tvOn();
     }
 });
 
 document.querySelector('.eventListenerGames').addEventListener('click', function () {
     if (monitor.onOff) {
-        screen.innerHTML = '<table><tbody>';
+        screen.innerHTML = `<h3>GAME SHOP</h3>`;
+        let a = '<table><tbody>';
         console.log(`1 - ${screen.innerHTML}`);
         games.forEach((game) => {
-            screen.innerHTML += `
+            a += `
                 <tr>
                     <td class="image"><img src="${game.image}" alt="game image"/></td>
                     <td class="name">${game.name}</td>
@@ -84,14 +86,14 @@ document.querySelector('.eventListenerGames').addEventListener('click', function
             `;
             console.log(`2 - ${screen.innerHTML}`);
         });
-        screen.innerHTML += '</tbody></table>';
+        a += '</tbody></table>';
+        screen.innerHTML += a;
         console.log(`3 - ${screen.innerHTML}`);
     }
 });
 
 document.querySelector('.eventListenerGoogle').addEventListener('click', function () {
     if (monitor.onOff) {
-        screen.innerHTML = '<iframe width="100%" height="315" src="../landing_page/index.html" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-        console.log(`Welcome ${screen}`);
+        screen.innerHTML = '<iframe width="100%" height="315" src="https://google.com/" title="Google" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
     }
 });
