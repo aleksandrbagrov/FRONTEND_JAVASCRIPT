48c12cfb392b8a24f96d303bcfa7e1216ad9e193
aleksandrbagrov <101454330+aleksandrbagrov@users.noreply.github.com>
2023-03-10 13:26:26 +0200

    add 2023_03_07

--- 2023_03_07/index.js
+++ 2023_03_07/index.js
@@ -25,14 +25,42 @@ const rand = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const rowPrinting = () => {
-    const rowArr = [];
-    for (let i = 0; i < columns; i += 1) {
-      if (i % 2 === 0) rowArr.push(rand(0, 65535));
-      rowArr.push(8192);
-    }
-    return console.log(chalk.bgBlack.green(String.fromCharCode(...rowArr)));
+const rowGeneration = () => {
+  const rowArr = [];
+  for (let i = 0; i < columns; i += 1) {
+    rowArr.push([rand(0, 1), rand(5, 10), rand(41, 122)]);
+  }
+  return rowArr;
+}
+
+const modifyStringArr = () => {
+  const modifyFirstRowArr = firstRowArr.map(([view, length,], i) => {
+    const newView = length === 0 ? rand(0, 10) : view;
+    const newLength = length === 0 ? rand(3, 9) : length - 1;
+    const newSimbol = newView ? 8192 : rand(41, 122); // : 8192;
+    firstRowArr[i] = [newView, newLength, newSimbol];
+    return [newView, newLength, newSimbol];
+  });
+  // console.log('Modified arr');
+  // console.log(modifyFirstRowArr);
+  // const modifyFirstRowArrString = modifyFirstRowArr.map(([,, simbol]) => simbol);
+  const modifyFirstRowArrString = modifyFirstRowArr.map(([, length, simbol]) => {
+    return simbol === 8192 ? ' ' : `${length}`
+  })
+    .join('');
+  // console.log(modifyFirstRowArrString);
+  // console.log(chalk.bgGreen.yellow.dim(String.fromCharCode(...modifyFirstRowArrString)));
+  console.log(chalk.bgGreen.yellow.dim(modifyFirstRowArrString));
 };
 
-setInterval(() => rowPrinting(), 500);
+const firstRowArr = rowGeneration();
+// console.log(firstRowArr);
+// const firstRowArrString = firstRowArr.map(([,, simbol]) => simbol);
+// console.log(firstRowArrString);
+console.log('This is a first row');
+modifyStringArr();
+// console.log(chalk.bgGreen.yellow(String.fromCharCode(...firstRowArrString)));
+
+
+setInterval(() => modifyStringArr(), 300);
 
